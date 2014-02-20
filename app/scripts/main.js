e543f5e9f74001eb962c50e2a20d8b944e773c15
Daniel Donaldson <donaldson.daniel@gmail.com>
2014-02-20 17:49:21 -0500

    2 tests written. One passing.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -38,4 +38,43 @@ $(document).ready(function(){
 		newMessage.save()
 	});
 
+
+	// Testing 
+	// Models created that will only be called during testing
+	window.testMessages = new TestMessagesCollection( );
+
+	testMessages.fetch({
+		success: function(){
+			tesMessages.each(function(testMessage){
+				new MessageView({model: testMessage});
+			})
+		},
+
+		error: function(){
+			console.log('Sorry, guys. Something has gone wrong.')
+		}
+	});
+
+	$('.js-test-send-button').click(function(){
+		console.log('You clicked a button')
+
+		var messageInstance = {
+			username: $('.username').val(),
+			messageText: $('.messageText').val(),
+			messageDate: Date.now(),
+			appId: 'TimeBandits'
+		};
+
+		console.log('Variable messageInstance was created')
+
+		var newMessage = messages.add(messageInstance);
+
+		console.log('User inputs pushed to messageInstance')
+
+		new MessageView({model: newMessage});
+		console.log('new MessageView created')
+
+		newMessage.save()
+	});
+	// End Testing
 })
--- test/spec/test.js
+++ test/spec/test.js
@@ -7,11 +7,32 @@
         describe('when the "Go!" button is clicked', function () {
             it('should render an additional div on the page', function () {
             	// Daniel
+            	var messagesBeforeTotal = $('.js-message-stream').find($('.message-box'));
+            	$('.username').val('Debra');
+            	$('.messageText').val('I got to party!');
+            	$('.js-test-send-button').click();
+            	var messagesAfterTotal = $('.js-message-stream').find($('.message-box'));
+            	// var firstTodoText = 'Grab some beer.';
+            	expect(messagesBeforeTotal.length).to.equal(messagesAfterTotal.length++);
             });
+
             it('should push an additional item to the server', function () {
             	// Jen
-            })('should throw an error if JavaScript is submitted in the input', function () {
+            });
+
+            it('should throw an error if JavaScript is submitted in the input', function () {
+            	// Daniel - holding off on this to discuss with Mason
+            });
+
+            it('should contain the value supplied in the input', function () {
             	// Daniel
+            	$('.username').val('Nancy');
+            	$('.js-test-send-button').click();
+            	var firstMessageText = $('.message-stream').find('.user-name').text();
+            	// var firstTodoText = 'Grab some beer.';
+            	expect(firstMessageText).to.contain('Nancy');
+            	// expect(true).to.equal(true);
+
             });
 
         });
