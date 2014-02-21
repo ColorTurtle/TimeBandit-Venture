ecbc25e1cd0114005e6c2e030d66e9d2d88e49c1
Jennifer Miller <jmiller6128@gmail.com>
2014-02-20 21:29:59 -0500

    model for login

--- app/scripts/models.js
+++ app/scripts/models.js
@@ -8,6 +8,6 @@ var Message = Backbone.Model.extend({
 var MessagesCollection = Backbone.Collection.extend({
 	model: Message,
 
-	//url: 'http://tiny-pizza-server.herokuapp.com/collections/messages'
-	url: 'http://0.0.0.0:3000/collections/messages'
+	url: 'http://tiny-pizza-server.herokuapp.com/collections/messages'
+
 })
