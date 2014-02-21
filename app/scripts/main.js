17816501a469fb1aeab6874a3c5d3665943e04dd
Jennifer Miller <jmiller6128@gmail.com>
2014-02-21 09:49:04 -0500

    fixed a bunch of mainjs, added keypress, logout working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,45 @@
 console.log('We have the best team! Go TimeBandits!');	
 
+
+//functions:
+submitNewUser = function(){
+	if($('#input-username').val() !== '') {
+	
+		var newUser = $('#input-username').val(); 
+
+		window.currentUser = {
+			username: newUser
+		};
+		
+		$('#myModal').modal('hide');
+
+		$('.message-parent').show();
+
+		$('.js-launch-chat').hide();
+		$('.js-logout-btn').show();
+	}
+	$('#input-username').val('');
+};
+
+submitNewMessage = function(){
+	if( $('#js-message-text').val() !== ''){
+		var awesomeMessage = $('#js-message-text').val();
+		
+		var msg = new Message({
+			username: currentUser.username,
+			messageText: awesomeMessage,
+		});
+
+		var newMessage = messages.add(msg);
+
+		new MessageView({model: newMessage});
+
+		newMessage.save();
+	}
+
+	$('#js-message-text').val('');
+};
+
 $(document).ready(function(){
 	window.messages = new MessagesCollection( );
 
@@ -12,49 +52,42 @@ $(document).ready(function(){
 
 		error: function(){
 			console.log('Sorry, guys. Something has gone wrong.')
-		}
-	});
+		},
+	}, {reset:true});
+	
+	//Possible to focus on input at appearance of modal?
+	// $('.js-user-input').hover(function(){		
+	// 	$('#input-username')focus();
+	// });
 
 	$('.js-start-chat').click(function(){
-		if($('#input-username').val() !== '') {
-			
-			var user = {
-				username: $('#input-username').val()
-			}
-			
-			var newUser = messages.add(user);
-
-			newUser.save();	
-			
-			$('#myModal').modal('hide');
-	
-			$('.message-parent').show(); //Potentially want to hide chat app at start
-		
-			$(".js-launch-chat").replaceWith("<h1>Welcome!</h1>");//maybe add username
-			$('.js-logout-btn').show();
-		}
-
-		$('#input-username').val('');
+		submitNewUser();
 	});
 
+	$('#input-username').on('keypress', function(btnPress){
+		if(btnPress.which === 13){
+			submitNewUser();
+		}
+	});
 
+	$('.js-logout-btn').click(function(){
+		if(confirm('Are you sure?')){
+			$('.message-parent').hide();
+			$(this).hide();	
+			$('.js-launch-chat').show();	
+		}
+	});
+
+	$('#js-message-text').on('keypress', function(btnPress){
+		if(btnPress.which === 13) {
+			submitNewMessage();
+		}
+	});
 
 	$('.js-send-button').click(function(){
-		console.log('You clicked a button')
-
-		var messageInstance = messages.last();
-		var newMessage = $('#js-message-text').val();
-		
-		messageInstance.set('messageText', newMessage);
-		messageInstance.set('messageDate', Date.now());
-		messageInstance.set('appId', 'TimeBandits');
-
-		var newMessage = messages.add(messageInstance);
-
-		new MessageView({model: newMessage});
-
-		newMessage.save()
+		submitNewMessage();
 	});
+
 });
 
 // Testing 
@@ -63,7 +96,7 @@ window.testMessages = new TestMessagesCollection( );
 
 testMessages.fetch({
 	success: function(){
-		tesMessages.each(function(testMessage){
+		testMessages.each(function(testMessage){
 			new MessageView({model: testMessage});
 		})
 	},
@@ -79,8 +112,6 @@ $('.js-test-send-button').click(function(){
 	var messageInstance = {
 		username: $('.username').val(),
 		messageText: $('.messageText').val(),
-		messageDate: Date.now(),
-		appId: 'TimeBandits'
 	};
 
 	console.log('Variable messageInstance was created')
@@ -95,5 +126,7 @@ $('.js-test-send-button').click(function(){
 	newMessage.save()
 });
 // End Testing
+
+
 
 
--- app/scripts/models.js
+++ app/scripts/models.js
@@ -1,25 +1,34 @@
 //collection of users for side div?
 
+	//var moment = require('moment');  
+
 var Message = Backbone.Model.extend({
 	idAttribute: '_id',
 
+	defaults:{
+		"username": "Fred",
+		"messageText": "'Sup?",
+		"messageDate": Date.now(),
+		"addId": "TimeBandits",
+		"options": {}
+	},
+
 	// Begin sanitizing input data before sending to database
 	initialize: function() {
-    _.each(this.attributes, function (val, key) {
-      this.set(key, this.sanitize(val));
-    }, this);
-  },
-  sanitize: function (str) { 
-    return _.escape(str) 
-  } 
-  // End sanitizing
+    	_.each(this.attributes, function (val, key) {
+      		this.set(key, this.sanitize(val));
+    	}, this);
+  	},
+  	sanitize: function (str) { 
+    	return _.escape(str) 
+  	} 
+  	// End sanitizing
 });
 
 var MessagesCollection = Backbone.Collection.extend({
 	model: Message,
 
-	//url: 'http://tiny-pizza-server.herokuapp.com/collections/messages'
-	url: 'http://0.0.0.0:3000/collections/messages'
+	url: 'http://tiny-pizza-server.herokuapp.com/collections/messages'
 });
 
 // For use in testing only
