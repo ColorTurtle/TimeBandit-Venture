686556c5457710cf9b7f506b6cb8fdebae0d4080
Jennifer Miller <jmiller6128@gmail.com>
2014-02-21 14:55:57 -0500

    expand height now avail

--- app/scripts/views.js
+++ app/scripts/views.js
@@ -17,15 +17,36 @@ var MessageView = Backbone.View.extend({
 	render: function(){
 		var renderedTemplate = this.createTemplate(this.model.attributes);
 
-
 		this.$el.html( renderedTemplate );
 	},
 
 	toggleExpandMessage: function(){
-		this.$el.toggleClass('expand');//some reason, this is not working
-		console.log('here?')//yes
+		this.$el.toggleClass('expand');
 	},
 
 	// Add second view here (when ready) to show
 	// team when username is clicked
+});
+
+
+var OldMessageView = Backbone.View.extend({
+	className: 'js-message-box message-box row',
+
+	createTemplate: _.template($('#message-template').text()),
+
+	events: {
+		"click": "toggleExpandMessage",
+	},
+
+	initialize: function(){
+		$('.js-message-stream').append(this.el);
+
+		this.render();
+	},	
+
+	render: function(){
+		var renderedTemplate = this.createTemplate(this.model.attributes);
+
+		this.$el.html( renderedTemplate );
+	}
 });
